7276afdec893fac0cee3224fd2e89df920c25133
Manirul Islam <mmimonir1234@gmail.com>
2024-03-31 15:04:06 +0600

    update 1505hrs

--- scripts/InchToFeet.js
+++ scripts/InchToFeet.js
@@ -3,7 +3,7 @@ const output = document.getElementById("output");
 
 function calculation() {
     const inch = input.value;
-    const feet = inch / 12;
+    const feet = (inch / 12).toFixed(2);
 
     output.innerText = feet;
 }
--- scripts/MilesToKm.js
+++ scripts/MilesToKm.js
@@ -1,9 +1,8 @@
-const input = document.getElementById("input");
 const output = document.getElementById("output");
 
 function calculation() {
     const miles = input.value;
-    const km = miles / 1.6093;
+    const km = (miles / 1.6093).toFixed(2);
 
     output.innerText = km;
 }
